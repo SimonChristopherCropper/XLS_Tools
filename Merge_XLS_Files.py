a464afa49b969010fee8a5932f5f1d6841e4ecc3
Simon Cropper <simon.cropper@bupa.com.au>
2019-04-16 16:36:01 +1000

    =Rant PyLint Static Code Analysis

--- Merge_XLS_Files.py
+++ Merge_XLS_Files.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-------------------------     Merge_XLS_Files()    --------------------------
+------------------------     merge_xls_files()    --------------------------
 
 This script is used to merge a directory of identically formatted xls/xlsx
 files and save the merged data to a csv in the same directory as the program.
@@ -10,13 +10,13 @@ files and save the merged data to a csv in the same directory as the program.
 A csv file is exported, rather than a spreadsheet, as some formatting
 is introduced by the python library.
 
-The program is run by typing "python Merge_XLS_Files.py" in the Ancaconda
+The program is run by typing "python merge_xls_files.py" in the Ancaconda
 console. A dialog will appear allowing you to select a directory to import.
 
 Assumptions
 1. Only tested in Windows OS
-2. All files should contain the same data. Column don't have to be in the 
-same order but need to have the exact same title. The order of columns will 
+2. All files should contain the same data. Column don't have to be in the
+same order but need to have the exact same title. The order of columns will
 match the first file imported.
 
 
@@ -28,92 +28,91 @@ Programmed by Simon Christopher Cropper 10 April 2018
 #--- IMPORTED MODULES
 #-----------------------------------------------------------------------
 
-import pandas as pd
-import numpy as np
 import glob
 import sys
 import os
+import tkinter as tk
 from tkinter import filedialog
-from tkinter import *
+import pandas as pd
 
 #-----------------------------------------------------------------------
 #--- DIRECTORY DIALOG
 #-----------------------------------------------------------------------
 
-root = Tk()
-root.withdraw()
-root.dirname =  filedialog.askdirectory()
+ROOT = tk.Tk()
+ROOT.withdraw()
+ROOT.dirname = filedialog.askdirectory()
 
 #-----------------------------------------------------------------------
 #--- MAIN PROGRAM
 #-----------------------------------------------------------------------
 
 # Establish where we putting the consolidated
-output_file_name = os.getcwd() + '\output_data\master'
-output_file_ext = '.csv'
+OUTPUT_FILE_NAME = os.getcwd() + '\\output_data\\master'
+OUTPUT_FILE_EXT = '.csv'
 
 # Continue if dialog returns a list of names
-if root.dirname:
+if ROOT.dirname:
 
-	# Some rudimentary feedbak
-	print('Selected "{}" directory to import'.format(root.dirname)) 
-	
-	# Create dataframe store data
-	all_data = pd.DataFrame()
-	
-	# Rudimentary trick to ensure headerf is only collected once
-	one_time = True
-	
-	# Create list of spreadsheets in directory
-	filelist = glob.glob(root.dirname + "/*.xls?")
-	
-	# Establish how many files were collated
-	n = len(filelist)
-	
-	# Establish number of sheets in first file
-	xlsx_file = pd.ExcelFile(filelist[1])
-	NumSheets = len(xlsx_file.sheet_names)
-	
-	# Let the user know what is going on
-	print(' ')
-	print("There are {} xlsx files in that directory".format(n))
-	
-	for SheetIndex in range(0, NumSheets):
-		
-		# Create an output file with sheet suffix
-		SheetLabel = str(SheetIndex + 1)
-		output_file = output_file_name + "_sheet_" + SheetLabel + output_file_ext
-		
-		# Let the user know what is going on
-		print(' ')
-		print('Importing data from Sheet {}...'.format(SheetLabel))
-		
-		# Reiterate through list of files
-		for f in filelist:
-			
-			# Create a visual cue to let the user know the program is still importing
-			sys.stdout.write('#')
-			sys.stdout.flush()
-			
-			# Record the header of the first file imported
-			if one_time :
-				df = pd.read_excel(f,ignore_index=True, sheet_name=SheetIndex) 
-				one_time = False
-			# Ignore the header on the remaining files
-			else :
-				df = pd.read_excel(f,ignore_index=True,sheet_name=SheetIndex,skip_row=0) 
-			
-			# Append the single file's data to the consolidated dataframe
-			all_data = all_data.append(df,ignore_index=True, sort=False) 
+    # Some rudimentary feedbak
+    print('Selected "{}" directory to import'.format(ROOT.dirname))
 
-		# Importing finished. Save data to CSV
-		all_data.to_csv(output_file, index=False)
-		
-		# User feedback
-		print(" ")
-		print('Data in sheet {} stored in "{}"'.format(SheetLabel, output_file))
+    # Create dataframe store data
+    ALL_DATA = pd.DataFrame()
+
+    # Rudimentary trick to ensure headerf is only collected once
+    ONE_TIME = True
+
+    # Create list of spreadsheets in directory
+    FILE_LIST = glob.glob(ROOT.dirname + "/*.xls?")
+
+    # Establish how many files were collated
+    N = len(FILE_LIST)
+
+    # Establish number of sheets in first file
+    XLSX_FILE = pd.ExcelFile(FILE_LIST[1])
+    NUM_SHEETS = len(XLSX_FILE.sheet_names)
+
+    # Let the user know what is going on
+    print(' ')
+    print("There are {} xlsx files in that directory".format(N))
+
+    for SheetIndex in range(0, NUM_SHEETS):
+
+        # Create an output file with sheet suffix
+        SheetLabel = str(SheetIndex + 1)
+        output_file = OUTPUT_FILE_NAME + "_sheet_" + SheetLabel + OUTPUT_FILE_EXT
+
+        # Let the user know what is going on
+        print(' ')
+        print('Importing data from Sheet {}...'.format(SheetLabel))
+
+        # Reiterate through list of files
+        for f in FILE_LIST:
+
+            # Create a visual cue to let the user know the program is still importing
+            sys.stdout.write('#')
+            sys.stdout.flush()
+
+            # Record the header of the first file imported
+            if ONE_TIME:
+                df = pd.read_excel(f, ignore_index=True, sheet_name=SheetIndex)
+                ONE_TIME = False
+            # Ignore the header on the remaining files
+            else:
+                df = pd.read_excel(f, ignore_index=True, sheet_name=SheetIndex, skip_row=0)
+
+            # Append the single file's data to the consolidated dataframe
+            ALL_DATA = ALL_DATA.append(df, ignore_index=True, sort=False)
+
+        # Importing finished. Save data to CSV
+        ALL_DATA.to_csv(output_file, index=False)
+
+        # User feedback
+        print(" ")
+        print('Data in sheet {} stored in "{}"'.format(SheetLabel, output_file))
 
 # Capture that dialog exited and returns no list
 else:
 
-	print("No directory selected. Bye.")
+    print("No directory selected. Bye.")
